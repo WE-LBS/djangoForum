3b0855062576f0e3463a7f13b2e3cfac86546292
WE-LBS <william.eliasson2@learnet.se>
2021-04-16 14:24:55 +0200

    test commit 2

--- forum/urls.py
+++ forum/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-# Test comment inside forum/urls.py
+# Test comment from forum/urls.py
 from django.contrib import admin
 from django.urls import path
 
