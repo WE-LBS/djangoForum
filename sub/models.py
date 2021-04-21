20f16234b8b51a5c38c3884b0610334ef0fddad3
WE-LBS <william.eliasson2@learnet.se>
2021-04-21 20:05:08 +0200

    fixed things

--- sub/models.py
+++ sub/models.py
@@ -1,15 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=400)
+    pub_date = models.DateTimeField(default=timezone.now)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,null=True)
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
     description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.description
--- sub/views.py
+++ sub/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
-from sub.models import Category
+from sub.models import Category, Post
 def category(request,category_from_url):
+    category = Category.objects.get(name=category_from_url)
+    posts = Post.objects.order_by("-pub_date").filter(category=category)
     context = {
-        "category" : Category.objects.get(name=category_from_url),
+        "category" : category,
+        "posts" : posts,
     }
     return render(request,"sub/category.html",context)
